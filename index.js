c4877040550596c5d58d97c839fca08c73150bde
Constantin Titarenko <constantin.titarenko@gmail.com>
2014-06-23 13:05:38 +0300

    Add update builder

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+var lookup = require('./lib/lookup');
 var findOrCreate = require('./lib/find-or-create');
+var update = require('./lib/update');
 var associate = require('./lib/associate');
 var aggregate = require('./lib/aggregate');
 
 module.exports = {
+	lookup: lookup,
 	findOrCreate: findOrCreate,
+	update: update,
 	associate: associate,
 	aggregate: aggregate
 };
