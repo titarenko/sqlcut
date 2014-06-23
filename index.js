d50d99b503ec4998da457285dc57aaeab6016921
Constantin Titarenko <constantin.titarenko@gmail.com>
2014-06-23 12:07:00 +0300

    Include aggregate function into exports

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var findOrCreate = require('./lib/find-or-create');
 var associate = require('./lib/associate');
+var aggregate = require('./lib/aggregate');
 
 module.exports = {
 	findOrCreate: findOrCreate,
-	associate: associate
+	associate: associate,
+	aggregate: aggregate
 };
